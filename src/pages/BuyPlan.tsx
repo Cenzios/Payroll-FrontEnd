b93a8fcd25100dd19af1fb8fc7fd3fc0743bdc39
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2026-01-06 12:51:36 +0530

    update3

--- src/pages/BuyPlan.tsx
+++ src/pages/BuyPlan.tsx
@@ -414,7 +414,7 @@ const BuyPlan = () => {
                     className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                   />
                   <label htmlFor="saveCard" className="ml-2 text-sm text-gray-600">
-                    Save card details
+                    Save card details 
                   </label>
                 </div>
 
