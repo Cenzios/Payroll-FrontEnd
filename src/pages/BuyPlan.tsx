9ab157f02634b0b7bd7346081a57e6926324ae4d
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2026-01-19 11:51:53 +0530

    debugiing

--- src/pages/BuyPlan.tsx
+++ src/pages/BuyPlan.tsx
@@ -12,6 +12,11 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from '../components/CheckoutForm';
 
+console.log(
+  'Stripe PK:',
+  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
+);
+
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY || '');
 
 const BuyPlan = () => {
